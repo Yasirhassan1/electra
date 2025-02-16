c16ab1c8702fc0f7c4507ea53a4c84e9a7a17930
Yasirhassan1 <yasuhassan7896@gmail.com>
2025-02-16 18:39:05 +0500

    Updated background color

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 export default function Card({pic, title, id, price, description, catagory}){
     return(
-      <Link className='card flex flex-col border border-yellow-600 bg-green-500 justify-between h-full w-full max-w-[300px] rounded-sm hover:shadow-xl transition-all duration-300 hover:scale-105 cursor-pointer' to= "/BuyPage" state={{pic, title, id, price, description, catagory}}> 
+      <Link className='card flex flex-col border border-yellow-600 justify-between h-full w-full max-w-[300px] bg-[#F9F9F9] rounded-sm hover:shadow-xl transition-all duration-300 hover:scale-105 cursor-pointer' to= "/BuyPage" state={{pic, title, id, price, description, catagory}}> 
       
         <div 
             className="pic w-full h-40 bg-center bg-contain bg-no-repeat"
